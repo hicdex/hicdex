ae8265d411252dbdfd9dc92686b8b3679444167a
Victor Felder <victor@draft.li>
2021-06-27 22:58:26 +0200

    fix for split contract reindex

--- src/hicdex/handlers/on_split_contract_origination_a.py
+++ src/hicdex/handlers/on_split_contract_origination_a.py
@@ -18,11 +18,11 @@ async def on_split_contract_origination_a(
         holder, _ = await models.Holder.get_or_create(address=contract_address)
         holder.is_split = True  # type: ignore
         await holder.save()
-        contract = await models.SplitContract.create(contract=holder, administrator=administrator, total_shares=total_shares)
+        contract, _ = await models.SplitContract.get_or_create(contract=holder, administrator=administrator, total_shares=total_shares)
 
         for address, share in shares.items():
             holder, _ = await models.Holder.get_or_create(address=address)
             holder_type = models.ShareholderStatus.benefactor
             if address in core_participants:
                 holder_type = models.ShareholderStatus.core_participant
-            await models.Shareholder.create(split_contract=contract, holder=holder, shares=int(share), holder_type=holder_type)
+            await models.Shareholder.get_or_create(split_contract=contract, holder=holder, shares=int(share), holder_type=holder_type)
